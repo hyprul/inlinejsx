d5bb451725942141d2d6f8638ab3620c6762e547
Sean Huang <hyprul@ucla.edu>
2019-11-28 17:32:00 -0600

    style change inline

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "blue";
+customStyle.fontSize = "50px";
+
 ReactDOM.render(
-  <h1 style={{ color: "red" }}>Hello World!</h1>,
+  <h1 style={customStyle}>Hello World!</h1>,
   document.getElementById("root")
 );
+/* A  A */
